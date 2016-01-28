ef434ef527128b76a4a127b64f5241f9dd162fdf
bonfrera <andrebonfrer@andre-bonfrer-imac.business.uq.edu.au>
2016-01-28 14:02:43 +1000

    Changed mcmod_make file but can ignore this since it will be superceded by next push from M Braun

--- data-raw/mcmod_make.R
+++ data-raw/mcmod_make.R
@@ -1,4 +1,43 @@
- mcmod <- mcmod(data.name = "dpp", brands.to_keep = c('HUGGIES','PAMPERS','LUVS','PL'), covv = c("avprc"), covnv = c("fracfnp","fracdnp","fracdist","numproducts"), T = 226,  N = 42, fweek = 1200, aggregated = FALSE)
+## Script to regenerate mcmod files 
+categories <- c("dpp","ptw","tti","ptw","lld")
+
+T = 226
+N = 42
+fweek = 1200
+covv = c("avprc")
+covnv = c("fracfnp","fracdnp","fracdist","numproducts")
+
+if(category=='lld') {
+	## liquid laundry detergents
+    brands.to_keep<-c('TIDE','ALL','PUREX','GAIN','CHEER','WISK','AH','PL')  # 91.7% share
+}
+
+if(category=='tti'){
+	## toilet tissue
+	brands.to_keep<-c('CHARMIN','QUILTEDNORTHERN','SCOTT','ANGELSOFT','PL') # (including Kleenex) 93.0% of share
+}
+
+if(category=='fti'){
+    ## facial tissue 
+    brands.to_keep<-c('KLEENEX','PUFFS','PL') # 93.0% of share
+}
+
+if(category=='dpp'){
+	## disposable diapers 
+    brands.to_keep<-c('HUGGIES','PAMPERS','LUVS','PL')  # 98.2% of share
+}
+
+if(category=='ptw'){
+	## paper towels
+    brands.to_keep<-c('BOUNTY','BRAWNY','SCOTT','VIVA','SPARKLE','PL') # 94.2% of share
+}
+
+## loop through categories
+
+for(category in categories) {
+
+	mcmod <- mcmodf(data.name = category, brands.to_keep = brands.to_keep, covv = covv, covnv = covnv, T = T,  N = N, fweek = fweek, aggregated = FALSE)
  
- save(mcmod, file="data/mcmoddpp.RData")
- 
+	save(mcmod, file=paste0("data/", mcmoddpp.RData")
+
+}
