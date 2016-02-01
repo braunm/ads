f057e669797fb9cbd78bab67d04249a0ed7b59bd
bonfrera <andrebonfrer@andre-bonfrer-imac.business.uq.edu.au>
2016-02-02 09:43:19 +1000

    Changing JbE to be equivalent to Jb and having a column of zeros in E which implies a missing value for that brand if it did not change creatives

--- R/ads-utilities.R
+++ R/ads-utilities.R
@@ -126,9 +126,12 @@ mcmodf <- function(data.name = "dpp", brands.to_keep = c('HUGGIES','PAMPERS','LU
     .brands_changed <- which(colSums(s.nnc)>0)
     brands_nnc <- brands.adv[.brands_changed]
 
-    # Data for creatives - note if any adv = 0 then columns removed
-    E <- s.nnc[1:T,.brands_changed]
-    JbE <- length(brands_nnc)
+    ## Data for creatives - note if any adv = 0 then columns removed
+    ## 2/2/2015 - now E is same dimension as ads (Jb)
+    ##    E <- s.nnc[1:T,.brands_changed]
+    E <- s.nnc[1:T,]
+    ##JbE <- length(brands_nnc)
+    JbE <- ncol(E)
     JbEv <- match(brands_nnc, brands.adv)			# pointer to brands.adv, which ones changed
 
 
